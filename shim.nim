e93828bd75c7d8005d2340e69720f5c48c6fa7eb
lufte <javierayres@gmail.com>
2018-09-06 22:11:08 -0300

    Add more content of the main function

--- shim.nim
+++ shim.nim
@@ -1,10 +1,10 @@
 # Userify shim
 # Copyright (c) 2018 Userify Corp
 
-import times, strutils
+import times, strutils, random, os
 
-let line_spacer = "\n******************************"
-let shim_version = "04012016-1"
+let lineSpacer = "\n******************************"
+let shimVersion = "04012016-1"
 
 proc parsePasswd(): seq[seq[string]] =
   var passwds: seq[seq[string]] = @[]
@@ -20,7 +20,18 @@ proc main(): int =
   return 1
 
 when isMainModule:
-  echo(line_spacer)
-  echo("[shim] $1 start: $2" % [shim_version, $(times.now())])
-  let s = times.cpuTime()
-  let timeToWait = main()
+  echo(lineSpacer)
+  echo("[shim] $1 start: $2" % [shimVersion, $(times.now())])
+  let startTime = times.cpuTime()
+  var timeToWait = 0
+  try:
+    timeToWait = main()
+  except:
+    echo(lineSpacer)
+    echo("Error: ", getCurrentExceptionMsg())
+    echo(lineSpacer)
+    timeToWait = toInt(30.0 + 60.0 * rand(1.0))
+  let elapsed = times.cpuTime() - startTime
+  if elapsed < timeToWait:
+    echo("[shim] sleeping: ", timeToWait - elapsed)
+    sleep(1000 * (timeToWait - elapsed))
